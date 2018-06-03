f547266e10d743a44b3ead6633be30d30e71fc8f
Jon <jon@jons-mbp.lan>
2018-06-03 15:16:43 -0400

    attribute builder w/ test

--- spec/templater.spec.js
+++ spec/templater.spec.js
@@ -1,7 +1,17 @@
 "use strict";
 
-describe("A suite", function() {
-    it("contains spec with an expectation", function() {
-        expect(true).toBe(true);
+const templater = require("../src/templater");
+
+describe("Attribute builder", () => {
+    it("transforms a JS object into an html5 attribute string", () => {
+        expect(templater.attributeBuilder(attributes)).toBe(attributeString);
     });
 });
+
+const attributes = {
+    htmlClass: "warning big-button",
+    id: "my-button",
+    href: "www.google.com"
+};
+
+const attributeString = 'class="warning big-button" id="my-button" href="www.google.com"';
--- src/templater.js
+++ src/templater.js
@@ -1,1 +1,37 @@
 "use strict";
+
+function elementBuilder(name, attributes, content) {
+    return `
+    ${openingTag(name, attributes)}
+    ${content}
+    ${closingTag(name)}
+    `;
+}
+
+function openingTag(name, attributes) {
+    const combined = `${name} `
+}
+
+function closingTag(name) {
+
+}
+
+function attributeBuilder(attributes) {
+    return Object.keys(attributes).map(a => {
+        const key = reservedKeywords.includes(a) ?  keywordResolver(a) : a;
+        return `${key}="${attributes[a]}"`;
+    }).join(" ");
+}
+
+const selfClosing = ["area", "base", "br", "col", "embed", "hr", "img", "input", 
+    "keygen", "link", "menuitem", "meta", "param", "source", "track", "wbr"];
+
+const reservedKeywords = ["htmlClass", "htmlFor"];
+
+function keywordResolver(key) {
+    return key.replace("html", "").toLowerCase();
+}
+
+module.exports = {
+    elementBuilder, openingTag, closingTag, attributeBuilder
+};
