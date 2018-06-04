cc4eb1939b1365e5a489cf3128926b1356e16e9d
Jon <jon@jons-mbp.lan>
2018-06-04 02:27:31 -0400

    tested event binding

--- app.js
+++ app.js
@@ -5,12 +5,17 @@ document.addEventListener("DOMContentLoaded", () => {
 function makeContent() {
     const h1 = templater.elementBuilder("h1", {}, "Hello birdFeet");
     const p = templater.elementBuilder("p", {}, "Little text.");
-    
+    const button = templater.elementBuilder("button", {onClick: "testClick();"}, "Click here.");
+
     return div({htmlClass: "main", style: "color: #4179d3"},
-        h1, p
+        h1, p, button
     );
 }
 
 function div(attributes, ...content) {
     return templater.elementBuilder("div", attributes, ...content);
+}
+
+function testClick() {
+    alert("hello");
 }
